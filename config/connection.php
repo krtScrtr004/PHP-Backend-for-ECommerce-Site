beab8454b919eb0f69ea65147d01eed914bfff21
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-01 15:21:38 +0800

    conf: create database connection class

---
+++ config/connection.php
@@ -0,0 +1,34 @@
+<?php
+
+class DBConnection {
+    private static $info = [
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'rest',
+    ];
+
+    private static $connection = null;
+
+    private function __construct() {
+        try {
+            $dsn = 'mysql: hostname=' . self::$info['hostname'] . '; dbname=' . self::$info['dbname'] . '; charset=utf8mb4';
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ];
+            self::$connection = new PDO($dsn, self::$info['username'], self::$info['password'], $options);
+        } catch (PDOException $e) {
+            http_response_code(500);
+            die($e->getMessage());
+        }
+    }
+
+    public static function getConnection() : PDO {
+        if (self::$connection === null) 
+            new self();
+
+        return self::$connection;
+    }
+}
